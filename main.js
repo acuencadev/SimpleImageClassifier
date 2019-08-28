0dd4d79daf3f2459738a19f17f9c8b4bcb59eb59
Amador Cuenca <sphi02ac@gmail.com>
2019-08-28 18:47:06 -0500

    Redesign the app with Vue

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-let net;
-
-async function app() {
-    console.log("Loading mobilenet...");
-
-    net = await mobilenet.load();
-
-    console.log("Successfully loaded model.");
-
-    const imgEl = document.getElementById("img");
-    const result = await net.classify(imgEl);
-
-    console.log(result);
-
-    document.getElementById("console").innerText = `
-      Prediction: ${result[0].className}\n
-      Probability: ${result[0].probability}
-    `;
-}
-
-var loadFile = function (event) {
-  var image = document.getElementById("img");
-  image.src = URL.createObjectURL(event.target.files[0]);
-};
+var app = new Vue({
+  el: '#app',
+  data: {
+    image: '',
+    loading: false,
+    predictions: []
+  }
+});
