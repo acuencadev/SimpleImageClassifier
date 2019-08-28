811e8e99d000e0a81ca63b7ec18baa9015d14c94
Amador Cuenca <sphi02ac@gmail.com>
2019-08-28 18:53:35 -0500

    Display/Hide the image

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    image: '',
-    loading: false,
-    predictions: []
-  }
+    el: '#app',
+    data: {
+        image: '',
+        loading: false,
+        predictions: []
+    },
+    methods: {
+        loadFile: function (e) {
+            var files = e.target.files || e.dataTransfer.files;
+
+            if (!files.length)
+                return;
+
+            this.createImage(files[0]);
+        },
+        createImage: function (file) {
+            var reader = new FileReader();
+            var vm = this;
+
+            reader.onload = (e) => {
+                vm.image = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        },
+        removeImage: function () {
+            this.image = '';
+            this.predictions = [];
+        }
+    }
 });
